81ee2022a566d8e9f4e3720c198626030f0b1e85
SerwahBonsu & Edwin & Nathaniel Clapp & srgupta <pair+SerwahBonsu.dwin357.nclapp.srgupta@devbootcamp.com>
2015-05-06 19:59:05 -0500

    Add feature to create question and post

--- app/controllers/controller.rb
+++ app/controllers/controller.rb
@@ -7,18 +7,40 @@ get '/' do
   ### redirect to login
 
   # OR
-
+erb :home
   #display all Q w/ A and comments  => this allows use of 'before' for authentication
 end
 
+# get '/hello' do
+#   erb :new
+# end
+
+get '/questions/new' do
+  # get form to add new question
+
+  erb :create_new_question
+end
+
+post '/questions' do
+  title = params[:title]
+  body = params[:body]
+  programming_language = params[:programming_language]
+  @question = Question.create!(title: title, body: body, programming_language: programming_language)
+
+  redirect ('/questions')
+end
+
+get '/questions' do
+  # Show all questions
+  @questions = Question.all
+  erb :show_questions
+
+end
 
 ###### USERS ######
 
 get '/users/login' do
   # what it says
-
-
-
 end
 
 get '/users/logout' do
@@ -58,25 +80,30 @@ end
 
 # redirect '/' unless current_user
 
-  @user = User.find_by_email(params[:email])
-  if @user.password == params[:password]
-    # sessions[:user] = @user.id
-  else
-    #redirect '/'
-  end
+  # @user = User.find_by_email(params[:email])
+  # if @user.password == params[:password]
+  #   # sessions[:user] = @user.id
+  # else
+  #   #redirect '/'
+  # end
 # end
 
 
 ####### AUTHENTICATED Q & A ######
 
-get '/questions/new' do
-  # get form to add new question
-end
+# get '/questions/new' do
+#   # get form to add new question
+#   # title = params[:title]
+#   # body = params[:body]
+#   # @question = Question.create(title: title, body: body)
+#   p "hello"
+#   erb :new
+# end
 
-post '/questions' do
-  # create new Q object in DB
-  # redirects /question
-end
+# post '/questions' do
+#   # create new Q object in DB
+#   # redirects /question
+# end
 
 # get '/questions/:id/answers' do
 #   # gets form to add a new ans to the specific Q
